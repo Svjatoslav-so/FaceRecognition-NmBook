19c63d0eb0b9daa53a7514e5eae933a8553a2d00
Святослав Сухоставский <s.svjatoslav.o@gmail.com>
2023-04-28 00:50:08 +0300

    В веб-приложении добавлена возможность просматривать группы собранные на основе работы с БД, через модуль dbManager.py
    Шаблоны разбиты по отдельным html файлам и собираются с помощью Jinja. Правка стилей. Добавлены соответствующие js-файлы.
    Пока что в приложении доступны обе версии просмотра групп (старый - через json, новый - через БД)

---
+++ web/static/js/smart-viewer-script.js
@@ -0,0 +1,69 @@
+console.log("Smart-viewer-script included successfully");
+let threshold_input = document.getElementById('threshold');
+let threshold_preview = document.getElementById('threshold_preview');
+let groups_li = document.getElementsByClassName('group_li');
+let origin_photo_block = document.getElementById('origin_photo_block');
+let viewer = document.getElementById('viewer');
+
+const options = { click: "toggleCover" };
+
+threshold_input.oninput = function(){
+    threshold_preview.innerHTML = threshold_input.value;
+}
+
+function reloadSmartViewer(){
+    let old_href = location.href;
+    if(old_href.includes('?')){
+        old_href = old_href.slice(0, old_href.indexOf('?'));
+    }
+    location.href = `${old_href}?threshold=${threshold_input.value}`;
+    
+}
+threshold_input.onchange = reloadSmartViewer;
+document.getElementById('db').onchange = function(){
+    console.log('Smart Script');
+    SendRequest('get',
+                '/set_db/'+ this.value,
+                '',
+                (Request) => console.log(JSON.parse(Request.response)));
+    reloadSmartViewer();
+}
+
+for(let i = 0; i < groups_li.length; i++){
+    groups_li[i].onclick = function(){loadGroup(this)};
+}
+
+/* Активирует Panzoom для всех текущих похожих фото */
+let activatePanzoom = function(){
+    const containers = document.querySelectorAll(".similar_figure .f-panzoom");
+    for(let i = 0;  i < containers.length; i++){
+        new Panzoom(containers[i], options);
+    }
+}
+
+function loadGroup(elem){
+    console.log(elem);
+    SendRequest('get',
+                '/get_group',
+                `threshold=${threshold_input.value}&origin_photo_id=${elem.dataset.origin_photoId}&origin_face_id=${elem.dataset.origin_faceId}&origin_photo_title=${elem.dataset.origin_photoTitle}&origin_photo_docs=${elem.dataset.origin_photoDocs}&origin_photo_x1=${elem.dataset.origin_photoX1}&origin_photo_y1=${elem.dataset.origin_photoY1}&origin_photo_x2=${elem.dataset.origin_photoX2}&origin_photo_y2=${elem.dataset.origin_photoY2}`,
+                (Request) => {
+                    data = JSON.parse(Request.response);
+                    console.log(data);
+                    origin_photo_block.innerHTML = data['origin_photo_block'];
+                    let originPhotoContainer = document.querySelector("#origin_photo_block .f-panzoom");
+                    new Panzoom(originPhotoContainer, options);
+                    viewer.innerHTML = data['view_panel'];
+                    activatePanzoom();
+                });
+    for(let i = 0; i < groups_li.length; i++){
+        if(groups_li[i].className.includes('active')){
+            groups_li[i].className = groups_li[i].className.replace(' active', '');
+        }
+    }
+    elem.className += ' active';
+
+}
+
+// on page load
+loadGroup(groups_li[0]);
+
--- web/web-app.py
+++ web/web-app.py
@@ -1,27 +1,48 @@
 import base64
 import json
+import os
+import pathlib
 from io import BytesIO
 from pathlib import Path
 
 import cv2
 from PIL import Image, ImageDraw, ImageOps
-from flask import Flask, render_template, request, abort, jsonify, send_from_directory
+from flask import Flask, render_template, request, abort, jsonify, send_from_directory, session, redirect, url_for
 
 import tool_module
+from dbManager import DBManager
+
+MIN_FACE_AREA = 650
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get('secret_key')
+
+
+@app.context_processor
+def common_context():
+    # определение пути
+    current_dir = pathlib.Path('./db/')
+    # определение шаблона
+    db_pattern = "*.db"
+    dbs = []
+    for file in current_dir.glob(db_pattern):
+        dbs.append(file.name.split('.')[0])
+    return dict(dbs=dbs)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html')
 
 
+# old
 @app.route("/group_viewer")
 def group_viewer():
     return render_template('group-viewer.html')
 
 
+# old
 @app.post("/open_group_file")
 def open_group_file():
     if request.method == 'POST':
@@ -40,6 +61,7 @@ def open_group_file():
         abort(401)
 
 
+# old
 @app.route("/get_img_with_face_area")
 def get_img_with_face_area():
     result = {}
@@ -76,12 +98,80 @@ def get_img_with_face_area():
     return jsonify(result)
 
 
+@app.route("/smart_group_viewer")
+def smart_group_viewer():
+    if request.method == 'GET':
+        # print('START', request)
+        threshold = request.args.get('threshold', 0.2)
+        # print('threshold', threshold)
+        manager = DBManager(f"./db/{session.get('db', common_context()['dbs'][0])}.db")
+        groups = manager.get_photo_group_list_of_similar(threshold, MIN_FACE_AREA)
+        # print('END')
+        return render_template('smart-viewer.html', **{'groups': groups,
+                                                       'threshold': threshold,
+                                                       })
+    return redirect(url_for('home'))
+
+
+@app.route("/get_group")
+def get_group():
+    if request.method == 'GET':
+        threshold = request.args.get('threshold', 0.2)
+        origin_photo_id = request.args.get('origin_photo_id', None)
+        origin_face_id = request.args.get('origin_face_id', None)
+        origin_photo_title = request.args.get('origin_photo_title', None)
+        origin_photo_docs = request.args.get('origin_photo_docs', None)
+        origin_photo_x1 = float(request.args.get('origin_photo_x1', 0))
+        origin_photo_y1 = float(request.args.get('origin_photo_y1', 0))
+        origin_photo_x2 = float(request.args.get('origin_photo_x2', 0))
+        origin_photo_y2 = float(request.args.get('origin_photo_y2', 0))
+
+        print(origin_face_id, origin_photo_title, origin_photo_docs)
+        manager = DBManager(f"./db/{session.get('db', common_context()['dbs'][0])}.db")
+        group = manager.get_group_photo_with_face(origin_face_id, threshold, MIN_FACE_AREA)
+        result = {
+            'origin_photo_block': render_template('origin-photo-block.html',
+                                                  **{'origin_photo_id': origin_photo_id,
+                                                     'origin_photo_title': origin_photo_title,
+                                                     'origin_photo_docs': origin_photo_docs,
+                                                     'origin_photo_x1': origin_photo_x1,
+                                                     'origin_photo_y1': origin_photo_y1,
+                                                     'origin_photo_x2': origin_photo_x2,
+                                                     'origin_photo_y2': origin_photo_y2,
+                                                     'foto_with_face': get_foto_with_square_around_face}),
+            'view_panel': render_template('view-panel.html',
+                                          **{'group': group,
+                                             'foto_with_face': get_foto_with_square_around_face})
+        }
+        return jsonify(result)
+    return redirect(url_for('home'))
+
+
 @app.route("/img_show/<path:path_to_img>")
 def img_show(path_to_img):
     path_to_img = str(Path(path_to_img))
     last_slash_index = path_to_img.rfind('\\')
     return send_from_directory(path_to_img[:last_slash_index + 1], path_to_img[last_slash_index + 1:],
                                as_attachment=True)
+
+
+@app.route("/set_db/<string:db>")
+def set_db(db):
+    session['db'] = db
+    return {"current_db": session['db']}
+
+
+def get_foto_with_square_around_face(photo_path, face_area):
+    img = Image.fromarray(cv2.cvtColor(cv2.imread(photo_path), cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    draw.rectangle(face_area, outline=(255, 0, 0), width=5)
+
+    if img.size[0] > 1500 or img.size[1] > 1500:
+        img = ImageOps.contain(img, (1500, 1500))
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = str(base64.b64encode(buffered.getvalue()))[2:-1]
+    return f'data:image/jpeg;base64,{img_str}'
 
 
 if __name__ == "__main__":
